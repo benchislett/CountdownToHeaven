e7c03a9f671740fb908cb550c7aed8ab7f565fb7
loan <loan@IC-RGL-LMC02.local>
2023-09-28 00:24:13 +0200

    Initial Commit

---
+++ countdown/src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+
+import Countdown from 'react-countdown';
+
+// Random component
+const Completionist = () => <h1 className='heading'>See you soon!<br/> I love you!</h1>;
+
+const HappyDate = new Date("November 23, 2023 17:30 UTC-4");
+// const HappyDate = Date.now() + 3000;
+
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a completed state
+    return <Completionist />;
+  } else {
+    // Render a countdown
+    return (<div>
+        <h1 className='heading'>
+          Countdown to Kat's Visit
+        </h1>
+        <div className='countdown-wrapper'>
+          <div className='countdown-box'>
+            {days}
+            <span className='legend'>Days</span>
+          </div>
+          <div className='countdown-box'>
+            {hours}
+            <span className='legend'>Hours</span>
+          </div>
+          <div className='countdown-box'>
+            {minutes}
+            <span className='legend'>Minutes</span>
+          </div>
+          <div className='countdown-box'>
+            {seconds}
+            <span className='legend'>Seconds</span>
+          </div>
+        </div>
+      </div>);
+  }
+};
+
+function App() {
+  return (
+    <div className="page">
+      <Countdown date={HappyDate} renderer={renderer}>
+        <Completionist />
+      </Countdown>
+    </div>
+  );
+}
+
+export default App;
